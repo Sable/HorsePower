f805d2260b58d91a962f20ef96cab8fa2ab10876
wukefe <wukefe@gmail.com>
2017-07-03 00:51:34 -0400

    type alias

---
+++ src/HorseIR/backend/h_global_type.h
@@ -0,0 +1,36 @@
+
+#define H_B 0
+#define H_H 1
+#define H_I 2
+#define H_L 3
+#define H_F 4
+#define H_E 5
+#define H_C 6
+#define H_S 7
+#define H_Y 8
+#define H_M 9
+#define H_D 10
+#define H_Z 11
+#define H_U 12
+#define H_W 13
+#define H_T 14
+#define H_X 15
+
+#define caseB case H_B:
+#define caseH case H_H:
+#define caseI case H_I:
+#define caseL case H_L:
+#define caseF case H_F:
+#define caseE case H_E:
+
+#define caseS case H_S:
+
+#define xp x->typ
+#define xb x->b
+#define xh x->h
+#define xi x->i32
+#define xl x->i64
+#define xf x->f32
+#define xe x->f64
+#define xs x->s
+
