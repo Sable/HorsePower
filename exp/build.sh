3aab493ab14126581ea6749ec5d5dff16c7b166d
wukefe <wukefe@gmail.com>
2017-11-22 23:57:49 -0500

    update front-end, with -O3

--- exp/build.sh
+++ exp/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+# build front-end
+(cd ../src/HorseIR/frontend && mkdir -p build && cd build && cmake ../ && make)
+
+# build back-end
 (cd ../src/HorseIR && make && ./horse)
 
