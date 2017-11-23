83cc96db3ba8a82f01800fd31231352b03697c3e
wukefe <wukefe@gmail.com>
2017-11-22 23:18:58 -0500

    add 2 libs in deploy_linux

--- libs/deploy_linux.sh
+++ libs/deploy_linux.sh
@@ -10,7 +10,7 @@ wget http://www.antlr.org/download/antlr4-cpp-runtime-4.7-source.zip
 ## Install antlr4
 unzip antlr4-cpp-runtime-4.7-source.zip -d antlr4-cpp
 mkdir -p antlr4
-(cd antlr4-cpp && mkdir run && cmake . && make && DESTDIR=../antlr4 make install)
+(cd antlr4-cpp && mkdir build && cd build && cmake ../ && make && DESTDIR=../../antlr4 make install)
 
 mv antlr4/usr/local/lib/* lib/
 mv antlr4/usr/local/include/* include
@@ -24,5 +24,18 @@ mkdir -p pcre2
 
 mv pcre2/pcre2-10.30/.libs/* lib/
 mv pcre2/pcre2-10.30/src/*.h include/ 
+
+## Download yaml-cpp
+wget https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.5.3.tar.gz
+tar -xf yaml-cpp-0.5.3.tar.gz
+mv yaml-cpp-0.5.3 yaml-cpp
+(cd yaml-cpp && mkdir build && cd build && cmake ../ && make)
+mv yaml-cpp/build/libyaml-cpp.a lib/
+mv yaml-cpp/include/yaml-cpp include/
+
+## Download spdlog
+wget https://github.com/gabime/spdlog/archive/v0.14.0.tar.gz
+tar -xf v0.14.0.tar.gz
+mv spdlog-0.14.0/include/spdlog include/
 
 
